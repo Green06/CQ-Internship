556bed50d5d9b37da8b3771eb24462463ccaebf1
christina <christinajacob06@gmail.com>
2023-07-04 12:19:04 +0530

    c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter string");
+string eg1 = Console.ReadLine();
+int eg2 = Convert.ToInt32(Console.ReadLine());
+char eg3 = Convert.ToChar(Console.ReadLine());
+bool eg4= Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("eg1"
+    + eg1);
+Console.WriteLine("eg2"+eg2);
+Console.WriteLine("eg3" + eg3);
+Console.WriteLine("eg4" + eg4);

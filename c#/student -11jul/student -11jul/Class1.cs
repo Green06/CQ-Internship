22a66eca179539478f806864d9cbd7a98581294b
christina <christinajacob06@gmail.com>
2023-07-11 18:35:10 +0530

    c#

--- c#/student -11jul/student -11jul/Class1.cs
+++ c#/student -11jul/student -11jul/Class1.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace student__11jul
 {
-    internal class Class1
+    
+        public class Student
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public char Grade
+        {
+            get
+            {
+                if (Mark >= 90)
+                    return 'A';
+                else if (Mark >= 80)
+                    return 'B';
+                else if (Mark >= 70)
+                    return 'C';
+                else
+                    return 'D';
+            }
+        }
+        /**public Employee(int id)
+        {
+            Id = id;
+        }
+        public Employee(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }**/
+        public Student(int id, string name, int mark)
+        {
+            Id = id;
+            Name = name;
+            Mark = mark;
+
+        }
+        public void display()
+        {
+            Console.WriteLine("id" + Id);
+            Console.WriteLine("name" + Name);
+            Console.WriteLine("mark" + Mark);
+            Console.WriteLine("grade" + Grade);
+        }
     }
 }
+
--- c#/student -11jul/student -11jul/Program.cs
+++ c#/student -11jul/student -11jul/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using student__11jul;
+Student stu1 = new Student(1,"jhon",85);
+stu1.display();
+Student stu2 = new Student(2, "david", 85);
+stu2.display();
+Student stu3 = new Student(3, "thomas", 85);
+stu3.display();

3b8529dfa34b8c91b958e9944ce96da938475983
christina <christinajacob06@gmail.com>
2023-07-18 09:35:49 +0530

    asg1

---
+++ c#/employee 17 july/employee 17 july/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+using employee_17_july;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee permanentEmployee = new Employee
+        {
+            Id = 1,
+            Name = "John Doe",
+            DateOfJoin = new DateTime(2022, 1, 15),
+            Department = "HR",
+            Salary = 50000.00,
+            JobType = 'P'
+        };
+
+        Employee contractEmployee = new Employee
+        {
+            Id = 2,
+            Name = "Jane Smith",
+            DateOfJoin = new DateTime(2022, 3, 10),
+            Department = "Finance",
+            Salary = 40000.00,
+            JobType = 'C'
+        };
+
+        IBonusCalculator permanentBonusCalculator = new PermanentEmployeeBonusCalculator();
+        IBonusCalculator contractBonusCalculator = new ContractEmployeeBonusCalculator();
+
+        double permanentEmployeeBonus = permanentBonusCalculator.CalculateBonus(permanentEmployee);
+        double contractEmployeeBonus = contractBonusCalculator.CalculateBonus(contractEmployee);
+
+        Console.WriteLine("Bonus for Permanent Employee: " + permanentEmployeeBonus);
+        Console.WriteLine("Bonus for Contract Employee: " + contractEmployeeBonus);
+    }
+}
+
---
+++ c#/employee 17 july/employee 17 july/emp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_17_july
+{
+
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime DateOfJoin { get; set; }
+            public string Department { get; set; }
+            public double Salary { get; set; }
+            public char JobType { get; set; }
+        }
+
+        public interface IBonusCalculator
+        {
+            double CalculateBonus(Employee employee);
+        }
+
+        public class PermanentEmployeeBonusCalculator : IBonusCalculator
+        {
+            public double CalculateBonus(Employee employee)
+            {
+                return employee.Salary * 0.20;
+            }
+        }
+
+
+        public class ContractEmployeeBonusCalculator : IBonusCalculator
+        {
+            public double CalculateBonus(Employee employee)
+            {
+                return employee.Salary * 0.15;
+            }
+        }
+
+    
+}

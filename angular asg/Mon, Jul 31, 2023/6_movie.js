16b24422151d320cb125f89427269e9c42fff58a
christina <christinajacob06@gmail.com>
2023-07-31 18:30:55 +0530

    ''

---
+++ angular asg/Mon, Jul 31, 2023/6_movie.js
@@ -0,0 +1,46 @@
+const movie= [
+    {
+      "MovieName": "The Great Adventure",
+      "ActorName": "John Smith",
+      "ReleaseDate": "2023-01-15"
+    },
+    {
+      "MovieName": "Mystery in the Woods",
+      "ActorName": "Emily Johnson",
+      "ReleaseDate": "2022-09-28"
+    },
+    {
+      "MovieName": "Love and Destiny",
+      "ActorName": "Michael Brown",
+      "ReleaseDate": "2023-05-02"
+    },
+    {
+      "MovieName": "City of Shadows",
+      "ActorName": "Sophia Williams",
+      "ReleaseDate": "2023-03-12"
+    },
+    {
+      "MovieName": "The Last Stand",
+      "ActorName": "William Davis",
+      "ReleaseDate": "2022-11-07"
+    },
+    {
+      "MovieName": "Echoes of Time",
+      "ActorName": "Olivia Wilson",
+      "ReleaseDate": "2022-12-19"
+    }
+  ]
+
+
+movie.push(
+    {
+        "MovieName": "The Final Stage",
+        "ActorName": "John Doe",
+        "ReleaseDate": "2022-08-11"
+  }
+
+)
+
+console.log(movie)
+
+
